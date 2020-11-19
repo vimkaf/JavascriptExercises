43427f09fe4ca754cf2d56962d58aed0626ae188
Kay Adamolekun <vimkaf@gmail.com>
2020-11-19 15:17:08 +0100

    Update exercise1.js

--- exercise1.js
+++ exercise1.js
@@ -1,5 +1,5 @@
 /**
- * Write a JavaScript program which iterates the integers from 1 to 100. But for multiples of three print "Fizz" instead of the number and for the multiples of five print "Buzz". For numbers which are multiples of both three and five print "FizzBuzz"
+ * 1. Write a JavaScript program which iterates the integers from 1 to 100. But for multiples of three print "Fizz" instead of the number and for the multiples of five print "Buzz". For numbers which are multiples of both three and five print "FizzBuzz"
  * 
  * 
  * 2. Write a JavaScript program to construct the following pattern, using a nested for loop
@@ -8,6 +8,13 @@
  *     * * *  
  *     * * * *  
  *     * * * * * 
+ *3. Write a JavaScript program to sum the multiples of 3 and 5 under 1000
+ *
+ *4. Write a JavaScript for loop that will iterate from 0 to 15. For each iteration, it will check if the current number is odd or even, and display a message to the screen.
+ *   Sample Output :
+ *      "0 is even"
+ *      "1 is odd"
+ *      "2 is even"
  */
 //Solution by pascal
 
