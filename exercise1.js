aec4f089ce91d2b1ad394a0da0453ba004affed7
pascal6190 <pascalobiora73@gmail.com>
2020-11-19 14:29:58 +0100

    my answer
    answer by paschal

--- exercise1.js
+++ exercise1.js
@@ -1,3 +1,8 @@
 /**
  * Write a JavaScript program which iterates the integers from 1 to 100. But for multiples of three print "Fizz" instead of the number and for the multiples of five print "Buzz". For numbers which are multiples of both three and five print "FizzBuzz"
  */
+
+ ;
+ for (var a = 2; a >= 2;a++){
+
+ }
