4f631a9cfdba00bd17a2f89aa03cf926b5abc0b1
Kay Adams <vimkaf@gmail.com>
2020-11-19 14:34:58 +0100

    Added Question 2

--- exercise1.js
+++ exercise1.js
@@ -1,3 +1,11 @@
 /**
  * Write a JavaScript program which iterates the integers from 1 to 100. But for multiples of three print "Fizz" instead of the number and for the multiples of five print "Buzz". For numbers which are multiples of both three and five print "FizzBuzz"
+ * 
+ * 
+ * 2. Write a JavaScript program to construct the following pattern, using a nested for loop
+*  
+* *  
+* * *  
+* * * *  
+* * * * * 
  */
